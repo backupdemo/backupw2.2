7b50c465879b8c1d9b4e3426e53714d0e0e7f204
backupdemo <55157356+backupdemo@users.noreply.github.com>
2019-09-22 09:56:47 +0530

    45465

---
+++ script.js
@@ -0,0 +1,15 @@
+var window_width = $(window).width() - $('#object').width();
+
+var document_height = $(document).height() - $(window).height();
+
+$(function () {
+    $(window).scroll(function () {
+        var scroll_position = $(window).scrollTop();
+        var object_position_left = window_width * (scroll_position / document_height);
+		if(scroll_position<document_height/2){
+        $('#object').css({
+            'left': object_position_left
+        });
+		}
+    });
+});
